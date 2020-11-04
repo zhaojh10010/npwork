3f175a863efa657d2b7351dc57274ae211e0cf09
jasper <wmyjzzjh@gmail.com>
2020-11-05 00:59:29 +0800

    complete server and code client

---
+++ client.cpp
@@ -0,0 +1,63 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <netdb.h>
+#include <string.h>
+
+using namespace std;
+#define MAXDATASIZE 100
+#define SERVER_PORT 80
+
+int main(int argc, char *argv[]) {
+    int clifd, numbytes;
+    struct hostent* he;
+    struct sockaddr_in addr;
+    char buf[MAXDATASIZE];
+    if(argc!=2) {
+        cout << "Please input ip or domain name correctly" << endl;
+        exit(-1);
+    }
+    argv++;
+    //get address information from input
+    for(;*argv!=NULL;argv++) {
+        //when argv is ip address
+        if((inet_aton(*argv, &addr.sin_addr))!=0) {
+            he = gethostbyaddr(&addr.sin_addr,sizeof(struct in_addr),AF_INET);
+            cout << "Get IP Address: " << *argv << endl;
+        } else {
+            he = gethostbyname(*argv);
+            cout << "Get domain name: " << *argv << endl;
+        }
+        if(he==NULL) {
+            continue;
+        }
+    }
+    if(he==NULL) {
+        cout << "No address information found" << endl;
+        exit(-1);
+    }
+    cout << "Get address information successfully" << endl;
+    if((clifd=socket(AF_INET,SOCK_STREAM,0))==-1) {
+        perror("Create socket failed: ");
+        exit(-1);
+    }
+    bzero(&addr,sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = SERVER_PORT;
+    addr.sin_addr = *((struct in_addr*)he->h_addr);
+    cout << "Start to connect to server" << endl;
+    if(connect(clifd, (sockaddr*)&addr, sizeof(struct sockaddr))==-1) {
+        perror("Connect failed: ");
+        exit(-1);
+    }
+    cout << "Connected to the server: " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << endl;
+    if((numbytes=recv(clifd,buf,MAXDATASIZE,0))==-1) {
+        perror("Recv data error: ");
+        exit(-1);
+    }
+    buf[numbytes]='\0';
+    cout << buf << endl;
+    return 0;
+}
