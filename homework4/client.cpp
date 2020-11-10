0cb72c1f77c5d3e1b766339c2386a007b8301ea7
jasper <wmyjzzjh@gmail.com>
2020-11-11 07:19:42 +0800

    add IO multiplexing instance

---
+++ homework4/client.cpp
@@ -0,0 +1,116 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+#define MAXDATASIZE 100
+#define SERVER_PORT 8888
+
+int main(int argc, char *argv[]) {
+    int clifd, numbytes;
+    struct hostent* he;
+    struct sockaddr_in addr;
+    char buf[MAXDATASIZE];
+    char name[MAXDATASIZE];
+    char** pptr;
+    cout << "===============Client started===============" << endl;
+    if(argc!=2) {
+        cout << "Please input ip or domain name correctly." << endl;
+        exit(-1);
+    }
+    argv++;
+    bzero(&addr,sizeof(addr));
+    //get address information from input
+    for(;*argv!=NULL;argv++) {
+        //when argv is ip address
+        if((inet_aton(*argv, &addr.sin_addr))!=0) {
+            he = gethostbyaddr(&addr.sin_addr,sizeof(struct in_addr),AF_INET);
+            cout << "Get IP Address: " << *argv << endl;
+        } else {//when argv is domain name
+            he = gethostbyname(*argv);
+            cout << "Get domain name: " << *argv << endl;
+        }
+        if(he==NULL) {
+            continue;
+        }
+    }
+    if(he==NULL) {
+        cout << "No address information found." << endl;
+        exit(-1);
+    }
+    //Print server address information
+    cout << "Get address information as follows:" << endl;
+    cout << "- host name: " << he->h_name << endl;
+    for (pptr = he->h_aliases; *pptr!=NULL; pptr++) {
+        cout << "- alias: " << *pptr << endl;
+    }
+    for (pptr = he->h_addr_list; *pptr!=NULL; pptr++) {
+        cout << "- ip address: " << inet_ntoa(*(in_addr*) (*pptr)) << endl;
+    }
+    cout << "============================================" << endl;
+    if((clifd=socket(AF_INET,SOCK_STREAM,0))==-1) {
+        perror("Create socket failed: ");
+        exit(-1);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(SERVER_PORT);
+    addr.sin_addr = *((struct in_addr*)he->h_addr);//h_addr is the first address in h_addr_list
+    // cout << "Start connecting to server" << endl;
+    if(connect(clifd, (sockaddr*)&addr, sizeof(struct sockaddr))==-1) {
+        perror("Connecting failed: ");
+        exit(-1);
+    }
+    cout << "Connected to the server "<< *(--argv) << ":" << SERVER_PORT << endl;
+    cout << "local address: " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << endl;
+    //transfer client name
+    cout << "Please input your name, and press ENTER to send(no more than 100 characters):\nYour name: ";
+    cin.getline(name,MAXDATASIZE);
+    if(strlen(name)==0)
+        strcpy(name,"anonymous");
+    if((numbytes=send(clifd,name,MAXDATASIZE,0))==-1) {
+        perror("Send name error: ");
+        exit(-1);
+    }
+    cout << "============================================" << endl;
+    // cout << "Receving msg from server: " << endl;
+    if((numbytes=recv(clifd,buf,MAXDATASIZE,0))<=0) {
+        perror("Recv data error: ");
+        exit(-1);
+    }
+    //buf[numbytes]='\0';
+
+    cout << "Server: " << buf << endl;
+    //read message from command line
+    cout << "============================================" << endl;
+    while(1) {
+        cout << "Input your message, and press ENTER to send(no more than 100 characters):\n" << name <<":";
+        bzero(buf,MAXDATASIZE);
+        cin.getline(buf,MAXDATASIZE);
+        if(cin.eof()) {//recognize Ctrl+D
+            cout << "(Input interrupted)" << endl;
+            break;
+        }
+        if(strlen(buf)==0) continue;//recognize ENTER
+        if((numbytes=send(clifd,buf,MAXDATASIZE,0))==-1) {
+            perror("Send data error: ");
+            exit(-1);
+        }
+        //reveive message from server
+        bzero(buf,MAXDATASIZE);
+        if((numbytes=recv(clifd,buf,MAXDATASIZE,0))==-1) {  
+            perror("Recv data error: ");
+            exit(-1);
+        } 
+        // cout << "received " << numbytes << " bits data" << endl;
+        cout << "Server:" << buf << endl;
+    }
+    cout << "===============Client ended================" << endl;
+    close(clifd);
+    return 0;
+}
---
+++ homework4/server.cpp
@@ -0,0 +1,206 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+
+using namespace std;
+#define BACKLOG_NUM 10 //maximum backlog num
+#define PORT 8888
+#define MAXDATASIZE 100
+#define MAXMESSAGESIZE 9999
+fd_set rset,allset;
+
+/**
+ * Process client request
+ * @param c client info
+ * @param buf buffer
+ */
+void process(struct c_info *c, char *buf);
+
+/**
+ * Close client and clear FD_SET bits
+ * @param c client info
+ */
+void close_cli(struct c_info *c);
+
+/**
+ * A client struct for thread
+ * @param connfd client socket descriptor
+ * @param name client name
+ * @param addr client ip address
+ * @param port client port
+ * @param msg  all messages in one session
+ * @param msgpos index of first idle area in msg
+ */
+struct c_info{
+    int connfd;
+    char *name;
+    char *addr;
+    int port;
+    char *msg;
+    int msgpos;
+};
+
+int main() {
+    int listenfd,connfd,clifd,maxfd,nready,i,maxi;
+    struct sockaddr_in servaddr,cliaddr;
+    socklen_t clilen;
+    c_info clients[FD_SETSIZE];
+    char buf[MAXDATASIZE];
+
+    for(i=0;i<FD_SETSIZE;i++)
+        clients[i].connfd = -1;
+    
+    cout << "======================Server started=======================" << endl;
+    //create socket
+    if((listenfd=socket(AF_INET, SOCK_STREAM,0)) ==-1 ) {
+        perror("Create server socket failed: ");
+        exit(-1);
+    }
+    maxfd = listenfd;
+    FD_ZERO(&allset);
+    FD_SET(listenfd,&allset);
+    
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    //Remeber all these two variables need to be converted
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+    //Set socket options
+    int opt = SO_REUSEADDR;
+    setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+    //bind the port
+    if(bind(listenfd, (sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
+	    perror("Bind port failed: ");
+        exit(-1);
+    }
+    cout << "Server started at port " << PORT << " successfully." << endl;
+    //listen to connections
+    if(listen(listenfd,BACKLOG_NUM) == -1) {
+        perror("Listen failed: ");
+        exit(-1);
+    }
+    cout << "Listening to connections..." << endl;
+    cout << "===========================================================" << endl;
+    while(1) {
+        bzero(&cliaddr,sizeof(cliaddr));
+        rset = allset;
+        if((nready = select(maxfd+1,&rset,NULL,NULL,NULL))<0) {
+            if(errno!=EINTR)
+                perror("Select error: ");
+            continue;
+        }
+        if(FD_ISSET(listenfd,&rset)) {
+            //wait to create conn
+            clilen = sizeof(cliaddr);//Here must alloc a variable or 'accept' cannot write into the clilen
+            if((connfd=accept(listenfd,(sockaddr*) &cliaddr, &clilen)) == -1) {
+                perror("Create connection failed: ");
+                continue;
+            }
+            //add connfd to clients array
+            for(i=0;i<FD_SETSIZE;i++)
+                if(clients[i].connfd==-1) {
+                    //Create thread and allocate private variable
+                    clients[i].connfd = connfd;
+                    clients[i].addr = inet_ntoa(cliaddr.sin_addr);
+                    clients[i].port = ntohs(cliaddr.sin_port);
+                    clients[i].name = new char[MAXDATASIZE];
+                    clients[i].msg = new char[MAXMESSAGESIZE];
+                    clients[i].msgpos = 0;
+                    break;
+                }
+            if(i==FD_SETSIZE) cout << "Warning: too many clients" << endl;
+            FD_SET(connfd,&allset);
+            if(connfd>maxfd) maxfd=connfd;
+            if(i>maxi) maxi=i;//record latest conn index
+            if(--nready<=0) continue;
+        }
+        for(i=0;i<=maxi;i++) {
+            bzero(buf,MAXDATASIZE);
+            struct c_info cinfo = clients[i];//浅拷贝???
+            if((clifd=cinfo.connfd)<0) continue;
+            if(FD_ISSET(clifd,&rset)) {
+                if(strlen(cinfo.name)==0) {//first connect
+                    //reveive client name
+                    if(recv(cinfo.connfd,cinfo.name,MAXDATASIZE,0)==-1) {
+                        perror("Receive client name failed: ");
+                        close_cli(&cinfo);
+                        break;
+                    }
+                    cout << "New connection from: " << cinfo.name << "@" << cinfo.addr
+                        << ":" << cinfo.port << endl;
+                    char welc[]="Welcome to my server,";
+                    strcat(welc,cinfo.name);
+                    if(send(connfd,welc,strlen(welc)+1,0)==-1) {
+                        perror("Send error: ");
+                        close_cli(&cinfo);
+                        break;
+                    }
+                }
+                
+                //process client request
+                // process(&cinfo,buf);
+                process(&clients[i],buf);
+                if(--nready<=0) break;
+            }
+        }
+    }
+    close(listenfd);
+    cout << "======================Server shutdown=======================" << endl;
+    return 0;
+}
+
+void process(struct c_info *c, char *buf) {
+    struct c_info cinfo = *c;
+    int connfd = cinfo.connfd;
+    
+    int numbytes,i,j;
+    if((numbytes=recv(connfd,buf,MAXDATASIZE,0))<=0) {
+        if(numbytes==0)
+            close(connfd);
+        close_cli(&cinfo);
+        return;
+    };
+    cout << cinfo.name << ":" << buf << endl;
+    
+    //Suppose the MAXMESSAGESIZE is suitable for a client in a session
+    //so ignore buffer overflow problem
+
+    //TODO pointer problem
+    int len = strlen(buf);
+    for(i=0;i<len;i++) {
+        cinfo.msg[cinfo.msgpos++] = buf[i];
+    }
+    cinfo.msg[cinfo.msgpos] = '\n';//add '\n' for every received msg
+    cinfo.msgpos++;
+    //send message back
+    if(send(connfd,buf,len+1,0)==-1) {
+        perror("Send message failed: ");
+        close_cli(&cinfo);
+        return;
+    }
+}
+
+void close_cli(struct c_info *c) {
+    struct c_info cinfo = *c;
+    FD_CLR(cinfo.connfd, &allset);
+    cout << "===========================================================" << endl;
+    cout << "Notice: Connection from " << cinfo.name << "@" << cinfo.addr
+            << ":" << cinfo.port << " closed."<< endl;
+    if(strlen(cinfo.msg)>0)
+        cout << "All data from " << cinfo.name << "@" << cinfo.addr
+            << ":" << cinfo.port << " are:\n" << cinfo.msg;
+    else cout << "No data transferred." << endl;
+    cout << "===========================================================" << endl;
+    cinfo.connfd=-1;
+    delete cinfo.name;
+    delete cinfo.msg;
+}
